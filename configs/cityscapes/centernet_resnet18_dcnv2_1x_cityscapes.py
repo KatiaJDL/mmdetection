5f98228ae63abee7a228a9a086fafd10f431bb22
KatiaJDL <zirconium2015@gmail.com>
2023-01-23 13:20:11 -0500

    centernet train pipeline

--- configs/cityscapes/centernet_resnet18_dcnv2_1x_cityscapes.py
+++ configs/cityscapes/centernet_resnet18_dcnv2_1x_cityscapes.py
@@ -37,6 +37,8 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
 train_pipeline = [
+    dict(type='LoadImageFromFile', to_float32=True, color_type='color'),
+    dict(type='LoadAnnotations', with_bbox=True),
     dict(
         type='PhotoMetricDistortion',
         brightness_delta=32,
@@ -52,10 +54,14 @@ train_pipeline = [
         to_rgb=True,
         test_pad_mode=None),
     dict(type='Resize', img_scale=(512, 512), keep_ratio=True),
-    dict(type='Normalize', **img_norm_cfg)
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
 
 test_pipeline = [
+    dict(type='LoadImageFromFile', to_float32=True),
     dict(
         type='MultiScaleFlipAug',
         scale_factor=1.0,
