bda6c7e576cae47da362af17920494d7297e0243
KatiaJDL <zirconium2015@gmail.com>
2023-01-24 16:35:19 -0500

    hourglass neck

---
+++ configs/centernet/centernet_hourglass104_dcnv2_140e_coco.py
@@ -0,0 +1,14 @@
+_base_ = './centernet_resnet18_dcnv2_140e_coco.py'
+
+work_dir = 'exp/centernet/cococ/hourglass_dcnv2_140e/'
+
+model = dict(
+    backbone=dict(
+        _delete_=True,
+        type='HourglassNet',
+        num_stacks=2, #2 for CenterNet, 1 for CenterPoly
+        norm_cfg=dict(type='BN', requires_grad=True),
+        pretrained=None,
+        init_cfg=None),
+    neck=None,
+    bbox_head=dict(in_channel=256))
